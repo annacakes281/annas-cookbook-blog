7d86f7be1d9b93ea5d5e9382b435ba416c81d978
annacakes281 <>
2023-09-26 13:30:44 +0000

    add further design to admin panel and add comment model to admin.py

--- recipe_site/admin.py
+++ recipe_site/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('recipe_name', 'slug', 'status', 'posted_on')
+    search_fields = ['recipe_name']
+    prepopulated_fields = {'slug': ('recipe_name', )}
+    list_filter = ('status', 'posted_on')
     summernote_fields = ('ingredients', 'recipe_steps')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'posted_on')
+    search_fields = ('name', 'email', 'body')
+    list_filter = ('name', 'posted_on')
