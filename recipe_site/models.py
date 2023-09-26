5f06783919cdb8ba69ed4235f8068951183e07c9
annacakes281 <>
2023-09-26 12:02:01 +0000

    create the inital post and comment models (may be edited later)

--- recipe_site/models.py
+++ recipe_site/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Posted"))
+
+
+class Post(models.Model):
+    recipe_name = models.CharField(max_length=200, unique=False)
+    slug = models.SlugField(max_length=200, unique=False)
+    recipe_image = CloudinaryField('image', default='placeholder')
+    written_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_posts")
+    edited_on = models.DateTimeField(auto_now=True)  # poss timefield
+    prep_time = models.CharField(max_length=30)  # poss timefield
+    cook_time = models.CharField(max_length=30)
+    ingredients = models.TextField()  # poss display as a list
+    recipe_steps = models.TextField()
+    excerpt = models.TextField(blank=True)  # unsure if needed
+    posted_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='recipe_likes', blank=True)
+
+    class Meta:
+        ordering = ['-posted_on']  # may change, Django docs
+
+    def __str__(self):
+        return self.recipe_name
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+# draft need to check in live view to edit comment model fields
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()  # unsure if needed
+    body = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['posted_on']  # may change, Django docs
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
