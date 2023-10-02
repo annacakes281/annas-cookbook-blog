be01adc812c4c6dcea504d19312fc7bb53541c5a
annacakes281 <>
2023-10-02 09:59:17 +0000

    add X_FRAME_OPTIONS so summernote runs on heroku

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -27,6 +27,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'SECRET_KEY'
 
+X_FRAME_OPTIONS = "SAMEORIGIN"  # Needed for Summernote to run in deployment
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
