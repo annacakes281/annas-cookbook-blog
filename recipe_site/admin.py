7209a2a90581f00de6ba6c385434afd05c86a6dd
annacakes281 <>
2023-10-02 11:21:45 +0000

    commet out admin.py to see if deployment works

--- recipe_site/admin.py
+++ recipe_site/admin.py
@@ -1,21 +1,21 @@
-from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
-from .models import Post, Comment
+# from django.contrib import admin
+# from django_summernote.admin import SummernoteModelAdmin
+# from .models import Post, Comment
 
 
-@admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):
+# @admin.register(Post)
+# class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('recipe_name', 'slug', 'status', 'posted_on')
-    search_fields = ['recipe_name']
-    prepopulated_fields = {'slug': ('recipe_name', )}
-    list_filter = ('status', 'posted_on')
-    summernote_fields = ('ingredients', 'recipe_steps',)
+#     list_display = ('recipe_name', 'slug', 'status', 'posted_on')
+#     search_fields = ['recipe_name']
+#     prepopulated_fields = {'slug': ('recipe_name', )}
+#     list_filter = ('status', 'posted_on')
+#     summernote_fields = ('ingredients', 'recipe_steps',)
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'body', 'post', 'posted_on')
-    search_fields = ('name', 'email', 'body')
-    list_filter = ('name', 'posted_on')
+#     list_display = ('name', 'body', 'post', 'posted_on')
+#     search_fields = ('name', 'email', 'body')
+#     list_filter = ('name', 'posted_on')
