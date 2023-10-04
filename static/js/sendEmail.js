db29b7028afd5e6ed15dbd61b3d2b61aa28b778a
annacakes281 <>
2023-10-04 19:12:51 +0000

    add js for the contact form (bug does not send, in test phase)

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("gmail", "cookbook-blog", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "recipe_idea": contactForm.recipeidea.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;  // To block from loading a new page
+}
